c99a6eba4baab466ca8c6e01f6de64dd397fc2a0
Ansh Kumar <anshkumar9686@gmail.com>
2024-11-20 19:54:57 +0530

    no comments

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,6 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    version: "0.8.8", // Specify the same version as in your Solidity file
+    version: "0.8.8", 
   },
 };
